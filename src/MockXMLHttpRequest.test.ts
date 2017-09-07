5ea3db6d7fcee8bcc4f000f33b8879187ccf2db1
James Newell <james@digitaledgeit.com.au>
2017-09-07 21:49:59 +1000

    added sync tests

--- src/MockXMLHttpRequest.test.ts
+++ src/MockXMLHttpRequest.test.ts
@@ -37,15 +37,188 @@ describe('MockXMLHttpRequest', () => {
     });
   });
 
+  describe('.getResponseHeader()', () => {
+    it('should have a response header', done => {
+      MockXMLHttpRequest.addHandler((req, res) => {
+        return res.header('Content-Type', 'application/json');
+      });
+
+      const xhr = new MockXMLHttpRequest();
+      xhr.open('get', '/');
+      xhr.onload = () => {
+        expect(xhr.getResponseHeader('Content-Type')).toEqual(
+          'application/json'
+        );
+        done();
+      };
+      xhr.onerror = failOnErrorEvent(done);
+      xhr.send();
+    });
+  });
+
+  describe('.getAllResponseHeaders()', () => {
+    it('should have a response header', done => {
+      MockXMLHttpRequest.addHandler((req, res) => {
+        return res
+          .header('Content-Type', 'application/json')
+          .header('X-Powered-By', 'SecretSauce');
+      });
+
+      const xhr = new MockXMLHttpRequest();
+      xhr.open('get', '/');
+      xhr.onload = () => {
+        expect(xhr.getAllResponseHeaders()).toEqual(
+          'content-type: application/json\r\nx-powered-by: SecretSauce\r\n'
+        );
+        done();
+      };
+      xhr.onerror = failOnErrorEvent(done);
+      xhr.send();
+    });
+  });
+
+  describe('.constructor()', () => {
+    it('should set .readyState to UNSENT', () => {
+      const xhr = new MockXMLHttpRequest();
+      expect(xhr.readyState).toEqual(MockXMLHttpRequest.UNSENT);
+    });
+  });
+
   describe('.open()', () => {
-    it('should be OPEN', () => {
+    it('should set .readyState to OPEN', () => {
       const xhr = new MockXMLHttpRequest();
       xhr.open('get', '/');
       expect(xhr.readyState).toEqual(MockXMLHttpRequest.OPENED);
     });
+
+    it('should call .onreadystatechange', () => {
+      const callback = jest.fn();
+      const xhr = new MockXMLHttpRequest();
+      xhr.onreadystatechange = callback;
+      xhr.open('get', '/');
+      expect(callback).toHaveBeenCalledTimes(1); //FIXME: check event
+    });
   });
 
   describe('.send()', () => {
+    const addListeners = (xhr: MockXMLHttpRequest, events: string[]) => {
+      const pushEvent = (event: MockEvent) => events.push(`xhr:${event.type}`);
+      xhr.addEventListener('readystatechange', pushEvent);
+      xhr.addEventListener('loadstart', pushEvent);
+      xhr.addEventListener('progress', pushEvent);
+      xhr.addEventListener('load', pushEvent);
+      xhr.addEventListener('loadend', pushEvent);
+
+      const uploadPushEvent = (event: MockEvent) =>
+        events.push(`upload:${event.type}`);
+      xhr.upload.addEventListener('loadstart', uploadPushEvent);
+      xhr.upload.addEventListener('progress', uploadPushEvent);
+      xhr.upload.addEventListener('load', uploadPushEvent);
+      xhr.upload.addEventListener('loadend', uploadPushEvent);
+    };
+
+    it('should throw an error when .open() has not been called', () => {
+      const xhr = new MockXMLHttpRequest();
+      expect(() => xhr.send()).toThrow();
+    });
+
+    describe('async=false', () => {
+      it('should dispatch events in order when request does not have a body', () => {
+        MockXMLHttpRequest.addHandler((req, res) => res);
+        const events: string[] = [];
+        const xhr = new MockXMLHttpRequest();
+        xhr.open('get', '/', false);
+        addListeners(xhr, events);
+        xhr.send();
+        expect(events).toEqual([
+          'xhr:loadstart',
+          'xhr:readystatechange', //HEADERS_RECEIVED
+          'xhr:readystatechange', //LOADING
+          'xhr:progress',
+          'xhr:progress',
+          'xhr:readystatechange', //DONE
+          'xhr:load',
+          'xhr:loadend'
+        ]);
+      });
+
+      it('should dispatch events in order when request has a body', () => {
+        MockXMLHttpRequest.addHandler((req, res) => res);
+        const events: string[] = [];
+        const xhr = new MockXMLHttpRequest();
+        xhr.open('put', '/', false);
+        addListeners(xhr, events);
+        xhr.send('hello world!');
+        expect(events).toEqual([
+          'xhr:loadstart',
+          'upload:loadstart',
+          'upload:progress',
+          'upload:progress',
+          'upload:load',
+          'upload:loadend',
+          'xhr:readystatechange', //HEADERS_RECEIVED
+          'xhr:readystatechange', //LOADING
+          'xhr:progress',
+          'xhr:progress',
+          'xhr:readystatechange', //DONE
+          'xhr:load',
+          'xhr:loadend'
+        ]);
+      });
+    });
+
+    describe('async=true', () => {
+      it('should dispatch events in order when request does not have a body', done => {
+        MockXMLHttpRequest.addHandler((req, res) => res);
+
+        const events: string[] = [];
+        const xhr = new MockXMLHttpRequest();
+        xhr.open('get', '/');
+        addListeners(xhr, events);
+        xhr.onloadend = () => {
+          expect(events).toEqual([
+            'xhr:loadstart',
+            'xhr:readystatechange', //HEADERS_RECEIVED
+            'xhr:readystatechange', //LOADING
+            'xhr:progress',
+            'xhr:progress',
+            'xhr:readystatechange', //DONE
+            'xhr:load'
+          ]);
+          done();
+        };
+        xhr.send();
+      });
+
+      it('should dispatch events in order when request has a body', done => {
+        MockXMLHttpRequest.addHandler((req, res) => res);
+
+        const events: string[] = [];
+        const xhr = new MockXMLHttpRequest();
+        xhr.open('put', '/');
+        addListeners(xhr, events);
+        xhr.onloadend = () => {
+          expect(events).toEqual([
+            'xhr:loadstart',
+            'upload:loadstart',
+            'upload:progress',
+            'upload:progress',
+            'upload:load',
+            'upload:loadend',
+            'xhr:readystatechange', //HEADERS_RECEIVED
+            'xhr:readystatechange', //LOADING
+            'xhr:progress',
+            'xhr:progress',
+            'xhr:readystatechange', //DONE
+            'xhr:load'
+          ]);
+          done();
+        };
+        xhr.send('hello world!');
+      });
+    });
+    //TODO: check values of all events
+
     it('should set the request body when X', done => {
       MockXMLHttpRequest.addHandler((req, res) => {
         expect(req.body()).toEqual('Hello World!');
@@ -116,144 +289,5 @@ describe('MockXMLHttpRequest', () => {
       xhr.send();
       xhr.abort();
     });
-  });
-
-  describe('.getResponseHeader()', () => {
-    it('should have a response header', done => {
-      MockXMLHttpRequest.addHandler((req, res) => {
-        return res.header('Content-Type', 'application/json');
-      });
-
-      const xhr = new MockXMLHttpRequest();
-      xhr.open('get', '/');
-      xhr.onload = () => {
-        expect(xhr.getResponseHeader('Content-Type')).toEqual(
-          'application/json'
-        );
-        done();
-      };
-      xhr.onerror = failOnErrorEvent(done);
-      xhr.send();
-    });
-  });
-
-  describe('.getAllResponseHeaders()', () => {
-    it('should have a response header', done => {
-      MockXMLHttpRequest.addHandler((req, res) => {
-        return res
-          .header('Content-Type', 'application/json')
-          .header('X-Powered-By', 'SecretSauce');
-      });
-
-      const xhr = new MockXMLHttpRequest();
-      xhr.open('get', '/');
-      xhr.onload = () => {
-        expect(xhr.getAllResponseHeaders()).toEqual(
-          'content-type: application/json\r\nx-powered-by: SecretSauce\r\n'
-        );
-        done();
-      };
-      xhr.onerror = failOnErrorEvent(done);
-      xhr.send();
-    });
-  });
-
-  describe('.constructor()', () => {
-    it('should set .readyState to UNSENT', () => {
-      const xhr = new MockXMLHttpRequest();
-      expect(xhr.readyState).toEqual(MockXMLHttpRequest.UNSENT);
-    });
-  });
-
-  describe('.open()', () => {
-    it('should set .readyState to OPEN', () => {
-      const xhr = new MockXMLHttpRequest();
-      xhr.open('get', '/');
-      expect(xhr.readyState).toEqual(MockXMLHttpRequest.OPENED);
-    });
-
-    it('should call .onreadystatechange', () => {
-      const callback = jest.fn();
-      const xhr = new MockXMLHttpRequest();
-      xhr.onreadystatechange = callback;
-      xhr.open('get', '/');
-      expect(callback).toHaveBeenCalledTimes(1); //FIXME: check event
-    });
-  });
-
-  describe('.send()', () => {
-    //TODO: add sync tests
-
-    it('should throw an error when .open() has not been called', () => {
-      const xhr = new MockXMLHttpRequest();
-      expect(() => xhr.send()).toThrow();
-    });
-
-    const addListeners = (xhr: MockXMLHttpRequest, events: string[]) => {
-      const pushEvent = (event: MockEvent) => events.push(`xhr:${event.type}`);
-      xhr.addEventListener('readystatechange', pushEvent);
-      xhr.addEventListener('loadstart', pushEvent);
-      xhr.addEventListener('progress', pushEvent);
-      xhr.addEventListener('load', pushEvent);
-      xhr.addEventListener('loadend', pushEvent);
-
-      const uploadPushEvent = (event: MockEvent) =>
-        events.push(`upload:${event.type}`);
-      xhr.upload.addEventListener('loadstart', uploadPushEvent);
-      xhr.upload.addEventListener('progress', uploadPushEvent);
-      xhr.upload.addEventListener('load', uploadPushEvent);
-      xhr.upload.addEventListener('loadend', uploadPushEvent);
-    };
-
-    it('should dispatch events in order when request does not have a body', done => {
-      MockXMLHttpRequest.addHandler((req, res) => res);
-
-      const events: string[] = [];
-      const xhr = new MockXMLHttpRequest();
-      xhr.open('get', '/');
-      addListeners(xhr, events);
-      xhr.onloadend = () => {
-        expect(events).toEqual([
-          'xhr:loadstart',
-          'xhr:readystatechange', //HEADERS_RECEIVED
-          'xhr:readystatechange', //LOADING
-          'xhr:progress',
-          'xhr:progress',
-          'xhr:readystatechange', //DONE
-          'xhr:load'
-        ]);
-        done();
-      };
-      xhr.send();
-    });
-
-    it('should dispatch events in order when request has a body', done => {
-      MockXMLHttpRequest.addHandler((req, res) => res);
-
-      const events: string[] = [];
-      const xhr = new MockXMLHttpRequest();
-      xhr.open('put', '/');
-      addListeners(xhr, events);
-      xhr.onloadend = () => {
-        expect(events).toEqual([
-          'xhr:loadstart',
-          'upload:loadstart',
-          'upload:progress',
-          'upload:progress',
-          'upload:load',
-          'upload:loadend',
-          'xhr:readystatechange', //HEADERS_RECEIVED
-          'xhr:readystatechange', //LOADING
-          'xhr:progress',
-          'xhr:progress',
-          'xhr:readystatechange', //DONE
-          'xhr:load'
-        ]);
-        done();
-      };
-      xhr.send('hello world!');
-    });
-
-    //TODO: check values of all events
   });
 });
