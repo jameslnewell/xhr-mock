51be73bf7e3f726ac0d51237a99268d39375e29f
James Newell <jnewell@atlassian.com>
2018-02-20 16:04:20 +1100

    ignore more things to make TS happy

--- packages/xhr-mock/src/MockXMLHttpRequestEventTarget.ts
+++ packages/xhr-mock/src/MockXMLHttpRequestEventTarget.ts
@@ -2,11 +2,12 @@ import MockEvent from './MockEvent';
 import MockProgressEvent from './MockProgressEvent';
 import MockEventTarget from './MockEventTarget';
 
+// @ts-ignore: https://github.com/jameslnewell/xhr-mock/issues/45
 export default class MockXMLHttpRequestEventTarget extends MockEventTarget
   implements XMLHttpRequestEventTarget {
   onabort: (this: XMLHttpRequestEventTarget, ev: ProgressEvent) => any;
   // @ts-ignore: https://github.com/jameslnewell/xhr-mock/issues/45
-  onerror: (this: XMLHttpRequestEventTarget, ev: ErrorEvent) => any;
+  onerror: (this: XMLHttpRequestEventTarget, ev: ProgressEvent) => any;
   onload: (this: XMLHttpRequestEventTarget, ev: ProgressEvent) => any;
   onloadend: (this: XMLHttpRequestEventTarget, ev: ProgressEvent) => any;
   onloadstart: (this: XMLHttpRequestEventTarget, ev: ProgressEvent) => any;
