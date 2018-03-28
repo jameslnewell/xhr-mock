af456cd6a13d71ed177990723b0dc54f040616bf
James Newell <jameslnewell@users.noreply.github.com>
2018-03-28 16:02:57 +1100

    bump the version and format the default error logging better (#59)
    * bump the version and format the default error logging better
    * pin typescript

--- packages/xhr-mock/src/handle.ts
+++ packages/xhr-mock/src/handle.ts
@@ -1,6 +1,11 @@
 import {MockFunction} from './types';
 import MockRequest from './MockRequest';
 import MockResponse from './MockResponse';
+import {MockError} from './MockError';
+
+const NO_RESPONSE_ERROR = new MockError(
+  'No handler returned a response for the request.'
+);
 
 function isPromise(arg: any): arg is Promise<MockResponse | undefined> {
   return arg && (arg as Promise<MockResponse | undefined>).then !== undefined;
@@ -16,14 +21,14 @@ export function sync(
 
     if (result) {
       if (isPromise(result)) {
-        throw new Error(
-          'xhr-mock: A MockFunction returned a Promise<MockResponse> in sync mode.'
+        throw new MockError(
+          'A handler returned a Promise<MockResponse> for a synchronous request.'
         );
       }
       return result;
     }
   }
-  throw new Error('xhr-mock: No handler returned a response.');
+  throw NO_RESPONSE_ERROR;
 }
 
 export function async(
@@ -44,7 +49,7 @@ export function async(
     )
     .then(result => {
       if (!result) {
-        throw new Error('xhr-mock: No handler returned a response.');
+        throw NO_RESPONSE_ERROR;
       }
       return result;
     });
