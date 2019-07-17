64b553482210821087996e7d20d26122f4dcd40c
James Newell <james@jameslnewell.dev>
2019-07-18 00:49:15 +1000

    got tests running

--- .jestrc.js
+++ .jestrc.js
@@ -3,17 +3,17 @@ module.exports = {
     {
       displayName: 'lint',
       preset: '@jameslnewell/jest-preset-lint',
-      testMatch: ['<rootDir>/**/(src|test)/**/*.test.ts?(x)'],
     },
     {
       displayName: 'type',
       preset: '@jameslnewell/jest-preset-type',
-      testMatch: ['<rootDir>/**/(src|test)/**/*.test.ts?(x)'],
     },
     {
       displayName: 'test',
       preset: '@jameslnewell/jest-preset-test',
-      testMatch: ['<rootDir>/**/(src|test)/**/*.test.ts?(x)'],
+      // moduleNameMapper: {
+      //   "^@xhr-mock/(.*)$": "<rootDir>/packages/$1/src"
+      // },
     },
   ],
 };
